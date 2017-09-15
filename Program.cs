1d981bce51622a309ccb45a4d9f584dfa8418c0f
Long Le <lelong37@gmail.com>
2017-09-15 14:43:23 -0500

    Update docs.

--- Program.cs
+++ Program.cs
@@ -6,18 +6,18 @@ namespace Chain.NetCore.Optimized
     {
         static void Main(string[] args)
         {
-            var middleWare1 = new MiddleWare1();
+            var middlewareEventArgs = new MiddlewareEventArgs()
+            {
+                Requests = new [] { 2, 5, 14, 22, 18, 3, 27, 20 }
+            };
 
+            var middleWare1 = new MiddleWare1();
+            
             middleWare1
                 .Use<MiddleWare2>()
                 .Use<MiddleWare3>()
                 .Use<MiddleWare4>()
                 .Use<MiddleWare5>();
-
-            var middlewareEventArgs = new MiddlewareEventArgs()
-            {
-                Requests = new [] { 2, 5, 14, 22, 18, 3, 27, 20 }
-            };
 
             middleWare1.Invoke(middlewareEventArgs);
         }
--- README.md
+++ README.md
@@ -11,6 +11,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        var middlewareEventArgs = new MiddlewareEventArgs()
+        {
+            Requests = new [] { 2, 5, 14, 22, 18, 3, 27, 20 }
+        };
+
         var middleWare1 = new MiddleWare1();
 
         middleWare1
@@ -18,11 +23,6 @@ class Program
             .Use<MiddleWare3>()
             .Use<MiddleWare4>()
             .Use<MiddleWare5>();
-
-        var middlewareEventArgs = new MiddlewareEventArgs()
-        {
-            Requests = new [] { 2, 5, 14, 22, 18, 3, 27, 20 }
-        };
 
         middleWare1.Invoke(middlewareEventArgs);
     }
